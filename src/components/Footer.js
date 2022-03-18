ac0c5246523951ac504773eb9c61596de096e93c
lingal <galumbauskasl@gmail.com>
2022-03-18 12:18:15 +0000

    desktop layout fully completed, added hover effects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,24 +5,24 @@ import './Footer.css';
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <footer className="footer grid-container">
-
-      <nav className="footer-nav grid-container">
+    <footer className="footer">
+      <div className="footer-content grid-container">
+        <nav className="footer-nav grid-container">
           <Logo></Logo>
-        
-        <div className="nav-items">
-          <a href="home">About</a>
-          <a href="home">Careers</a>
-          <a href="home">Events</a>
-          <a href="home">Products</a>
-          <a href="home">Support</a>
-        </div>
-      </nav>
 
-      
-      <div className="links">
-        <SocialLinks></SocialLinks>
-        <p> &copy; {year} Loopstudios. All rights reserved.</p>
+          <div className="nav-items">
+            <a href="home">About</a>
+            <a href="home">Careers</a>
+            <a href="home">Events</a>
+            <a href="home">Products</a>
+            <a href="home">Support</a>
+          </div>
+        </nav>
+
+        <div className="links">
+          <SocialLinks></SocialLinks>
+          <p> &copy; {year} Loopstudios. All rights reserved.</p>
+        </div>
       </div>
     </footer>
   );
